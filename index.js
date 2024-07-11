9ec6a3c69c9863e6314f7e9200c484960c7eaee3
Vaidas <vaidas.makstutis@gmail.com>
2024-07-11 16:45:57 +0300

    handling http get request

--- index.js
+++ index.js
@@ -3,17 +3,24 @@ import { PORT } from "./config.js";
 
 const app = express();
 
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" }
+];
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.get("/api/courses", (req, res) => {
-  res.send([1, 2, 3]);
+  res.send(courses);
 });
 
 app.get("/api/courses/:id", (req, res) => {
-  const { id } = req.params;
-  res.send(`Course: ${id}`);
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send("The course with the given ID was not found!");
+  res.send(course);
 });
 
 app.get("/api/posts/:year/:month", (req, res) => {
