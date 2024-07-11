2e113bb231f99e3d6ecc032645a6bd5692cf4231
Vaidas <vaidas.makstutis@gmail.com>
2024-07-11 16:34:44 +0300

    route parameters

--- index.js
+++ index.js
@@ -11,4 +11,13 @@ app.get("/api/courses", (req, res) => {
   res.send([1, 2, 3]);
 });
 
+app.get("/api/courses/:id", (req, res) => {
+  const { id } = req.params;
+  res.send(`Course: ${id}`);
+});
+
+app.get("/api/posts/:year/:month", (req, res) => {
+  res.send(req.params);
+});
+
 app.listen(PORT, () => console.log(`Listening on the port: ${PORT}`));
